509bc4f1987deb00e1d68929a9baa77daca60bab
Lieven Doclo <lieven@atomist.com>
2019-12-03 12:58:15 +0100

    Update refactors.kts

--- test/support/guava/transforms/refactors.kts
+++ test/support/guava/transforms/refactors.kts
@@ -13,6 +13,26 @@ fun MethodCallExpr.getQualifiedSignature(): String? {
     }
 }
 
+fun replaceStaticMethodWithSameArguments(qualifiedName: String, replacementClassFQN: String, replacementMethod: String): (CompilationUnit, File) -> Unit {
+    return { cu, file ->
+        run {
+            var changed = false
+            cu.findAll(MethodCallExpr::class.java)
+                    .filter { mce -> mce.getQualifiedSignature() == qualifiedName }
+                    .forEach { mce ->
+                        run {
+                            mce.setName(replacementMethod)
+                            mce.setScope(NameExpr(replacementClassFQN))
+                            changed = true;
+                        }
+                    }
+            if (changed) {
+                file.writeText(cu.toString(PrettyPrinterConfiguration()))
+            }
+        }
+    }
+}
+
 fun replaceMethodOnSameClass(qualifiedName: String, replacement: String): (CompilationUnit, File) -> Unit {
     return { cu, file ->
         run {
